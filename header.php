18b963e5aa94c00986f8146dcc4401a569dc4e6c
Steven Stern <webmaster@sterndata.com>
2015-09-29 10:13:11 -0500

    put header image below site brandding

--- header.php
+++ header.php
@@ -30,18 +30,17 @@
 		<div class="row">
 			<div class="site-header-inner col-sm-12">
 
-				<?php $header_image = get_header_image();
-				if ( ! empty( $header_image ) ) { ?>
-					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
-						<img src="<?php header_image(); ?>" width="<?php echo get_custom_header()->width; ?>" height="<?php echo get_custom_header()->height; ?>" alt="">
-					</a>
-				<?php } // end if ( ! empty( $header_image ) ) ?>
-
-
 				<div class="site-branding">
 					<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 					<p class="lead"><?php bloginfo( 'description' ); ?></p>
 				</div>
+
+                                <?php $header_image = get_header_image();
+                                if ( ! empty( $header_image ) ) { ?>
+                                        <a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
+                                                <img src="<?php header_image(); ?>" width="<?php echo get_custom_header()->width; ?>" height="<?php echo get_custom_header()->height; ?>" alt="">
+                                        </a>
+                                <?php } // end if ( ! empty( $header_image ) ) ?>
 
 			</div>
 		</div>
